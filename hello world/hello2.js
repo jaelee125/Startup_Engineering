0b56ef595c2b77476d1857c1324d9ad639d5207c
Lee, Jae <jae.lee01@sap.com>
2015-07-14 10:09:44 -0500

    removed unnecessary files

--- hello world/hello2.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "Modifiy this script to write out something different. \n";
-fs.writeFileSync(outfile, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
